5daf28d03a3e7ae449a55e3a12cfabe01c357c67
Nishkal <nishkal8463@gmail.com>
2021-10-08 19:36:29 +0530

    profile page ui setup

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,15 +1,89 @@
-import React from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View, TouchableOpacity, Image, TextInput} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
 import {signout} from '../actions/login';
+import profile1 from '../Public/Images/profile1.jpeg';
+import profile2 from '../Public/Images/profile2.jpeg';
+import profile3 from '../Public/Images/profile3.jpeg';
+import profile4 from '../Public/Images/profile4.jpeg';
+import profile5 from '../Public/Images/profile5.jpeg';
+import profile6 from '../Public/Images/profile6.jpeg';
+import profile7 from '../Public/Images/profile7.jpeg';
+import profile8 from '../Public/Images/profile8.jpeg';
+import profile9 from '../Public/Images/profile9.jpeg';
+import profile10 from '../Public/Images/profile10.jpeg';
+import profile11 from '../Public/Images/profile11.jpeg';
+import profile12 from '../Public/Images/profile12.jpeg';
+import profile13 from '../Public/Images/profile13.jpeg';
+import profile14 from '../Public/Images/profile14.jpeg';
+import styles from './styles';
 
 const Profile = () => {
+  const images = [
+    profile1,
+    profile1,
+    profile2,
+    profile3,
+    profile4,
+    profile5,
+    profile6,
+    profile7,
+    profile8,
+    profile9,
+    profile10,
+    profile11,
+    profile12,
+    profile13,
+    profile14,
+  ];
+
   const dispatch = useDispatch();
+  const userData = useSelector(state => state?.login?.authData?.result);
+
+  // useEffect(() => {
+  // }, [userData]);
+
   return (
-    <View>
-      <Text>{'Profile PAGE'}</Text>
-      <TouchableOpacity onPress={() => dispatch(signout())}>
-        <Text>Logout</Text>
+    <View style={styles.profileContainer}>
+      <View style={styles.profileImageContainer}>
+        <Image
+          style={styles.profileImage}
+          source={images[Math.floor(Math.random() * 14 + 1)]}
+        />
+      </View>
+      <View style={styles.profileInfoContainer}>
+        <View style={styles.singleInfoContainer}>
+          <Text style={styles.leftText}>First Name: </Text>
+          <Text numberOfLines={1} style={styles.rightText}>
+            {userData?.name?.split(' ')[0]}
+          </Text>
+        </View>
+        <View style={styles.singleInfoContainer}>
+          <Text style={styles.leftText1}>Last Name: </Text>
+          <Text numberOfLines={1} style={styles.rightText}>
+            {userData?.name?.split(' ')[1]}
+          </Text>
+        </View>
+        <View style={styles.singleInfoContainer}>
+          <Text style={styles.leftText2}>Email: </Text>
+          <Text numberOfLines={1} style={styles.rightText}>
+            {userData?.email}
+          </Text>
+        </View>
+      </View>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={styles.profileEditContainer}
+        disabled={true}
+        // onPress={() => dispatch(signout())}
+      >
+        <Text style={styles.buttonText}>Edit Profile</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        style={styles.profileLogoutContainer}
+        onPress={() => dispatch(signout())}>
+        <Text style={styles.buttonText}>Logout</Text>
       </TouchableOpacity>
     </View>
   );
---
+++ src/Profile/styles.js
@@ -0,0 +1,89 @@
+import {StyleSheet} from 'react-native';
+import * as Constants from '../utils/Constants';
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: Constants.APP_BACKGROUND_COLOR,
+  },
+  profileImageContainer: {
+    height: 230,
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  profileInfoContainer: {
+    height: 250,
+    marginBottom: 10,
+    justifyContent:"space-evenly",
+  },
+  profileImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 200 / 2,
+    overflow: 'hidden',
+    borderWidth: 1,
+    borderColor: Constants.ALERT_CONFIRM
+  },
+  singleInfoContainer: {
+    height: '15%',
+    display: 'flex',
+    flexDirection: 'row',
+    paddingLeft: 20,
+    alignItems: 'center',
+  },
+  leftText: {
+    color: Constants.APP_THEME_COLOR,
+    fontFamily: 'serif',
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginRight: 16,
+  },
+  rightText: {
+    width: '70%',
+    color: Constants.APP_THEME_COLOR,
+    fontFamily: 'serif',
+    fontSize: 17,
+  },
+  profileEditContainer: {
+    height: 35,
+    width: '50%',
+    borderRadius: 20,
+    backgroundColor: Constants.APP_THEME_COLOR,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginBottom:30
+  },
+  profileLogoutContainer: {
+    height: 35,
+    width: '50%',
+    borderRadius: 20,
+    backgroundColor: Constants.APP_THEME_COLOR,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  leftText1:{
+    color: Constants.APP_THEME_COLOR,
+    fontFamily: 'serif',
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginRight: 20,
+  },
+  leftText2:{
+    color: Constants.APP_THEME_COLOR,
+    fontFamily: 'serif',
+    fontSize: 17,
+    fontWeight: 'bold',
+    marginRight: 60,
+  },
+  buttonText: {
+    color: Constants.APP_BACKGROUND_COLOR,
+    fontFamily: 'serif',
+    fontSize: 17,
+  },
+});
+
+export default styles;
