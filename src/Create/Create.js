926af483adef79efe00fb5a01583021e3b5c7fd1
Nishkal <nishkal8463@gmail.com>
2021-12-03 16:54:56 +0530

    create page ui v1

--- src/Create/Create.js
+++ src/Create/Create.js
@@ -1,11 +1,83 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  SafeAreaView,
+} from 'react-native';
+import styles from './styles';
+import {useSelector, useDispatch} from 'react-redux';
 
 const Create = () => {
-
   return (
-    <View>
-      <Text>{'Create PAGE'}</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Create Memory</Text>
+      </View>
+      <View style={styles.bodyContainer}>
+        <View style={styles.inputViewContainer}>
+          <Text style={styles.inputText}>Title</Text>
+          <TextInput
+            name="Title"
+            type="text"
+            style={styles.boxContainer}
+            // value={formData.email}
+            // onChangeText={value => {
+            //   setFormData({...formData, email: value.trim()});
+            // }}
+          />
+        </View>
+        <View style={styles.largeInputViewContainer}>
+          <Text style={styles.inputText}>Message</Text>
+          <TextInput
+            name="Message"
+            type="text"
+            multiline={true}
+            style={styles.largeBoxContainer}
+            // value={formData.email}
+            // onChangeText={value => {
+            //   setFormData({...formData, email: value.trim()});
+            // }}
+          />
+        </View>
+        <View style={styles.inputViewContainer}>
+          <Text style={styles.inputText}>Tags</Text>
+          <TextInput
+            name="Tags"
+            type="text"
+            style={styles.boxContainer}
+            // value={formData.email}
+            // onChangeText={value => {
+            //   setFormData({...formData, email: value.trim()});
+            // }}
+          />
+        </View>
+        <View style={styles.inputViewContainer}>
+          <Text style={styles.inputText}>Upload Image</Text>
+          <TextInput
+            name="image"
+            type="text"
+            style={styles.boxContainer}
+            // value={formData.email}
+            // onChangeText={value => {
+            //   setFormData({...formData, email: value.trim()});
+            // }}
+          />
+        </View>
+        <View style={styles.inputViewContainer}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={styles.button1}
+            // onPress={() => {
+            //   validateData(formData);
+            // }}
+          >
+            <Text style={styles.buttonText}>Upload Image</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
---
+++ src/Create/styles.js
@@ -0,0 +1,73 @@
+import {StyleSheet} from 'react-native';
+import * as Constants from '../utils/Constants';
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+    backgroundColor: Constants.APP_BACKGROUND_COLOR, // Back ground color
+    // backgroundColor:"red"
+  },
+  headerContainer: {
+    height: '10%',
+    // backgroundColor: 'red',
+    justifyContent:"center",
+    alignItems:"center"
+
+  },
+  headerText: {
+    fontWeight:"bold",
+    fontSize:23,
+    color:Constants.APP_THEME_COLOR
+  },
+  inputViewContainer: {
+    height: '15%',
+    marginHorizontal: 15,
+    justifyContent: 'center',
+    marginBottom: "5%",
+  },
+  boxContainer: {
+    height: 50,
+    borderColor: Constants.APP_THEME_COLOR,
+    borderWidth: 1,
+    textAlignVertical: "top",
+    // color: Constants.APP_THEME_COLOR,
+  },
+  largeInputViewContainer: {
+    height: '20%',
+    marginHorizontal: 15,
+    justifyContent: 'center',
+    marginBottom: "5%",
+  },
+  largeBoxContainer:{
+    height: "60%",
+    borderColor: Constants.APP_THEME_COLOR,
+    borderWidth: 1,
+    textAlignVertical: "top",
+  },
+  bodyContainer: {
+    flex: 1,
+    // backgroundColor: 'blue',
+  },
+  inputText: {
+    color: Constants.APP_THEME_COLOR,
+    marginBottom: 10,
+  },
+  button1: {
+    height: 35,
+    width: '90%',
+    borderRadius: 20,
+    backgroundColor: Constants.APP_THEME_COLOR,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: Constants.APP_BACKGROUND_COLOR,
+    fontFamily: 'serif',
+    fontSize: 17,
+  },
+});
+
+export default styles;
